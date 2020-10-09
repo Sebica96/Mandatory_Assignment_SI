7656e1bc4a5b4803977fc9ae19ba14c99790387f
benazeerturkmani <bena0013@stud.kea.dk>
2020-10-09 18:06:03 +0200

    legacy system - POST request to http://localhost:8080/nemID - not yet fixed.

--- Legacy Central System/fileReader.py
+++ Legacy Central System/fileReader.py
@@ -2,12 +2,12 @@ import csv
 import random
 from xml.dom import minidom
 import os
-#from nemID_ESB import nemId
+# from nemID_ESB import nemId
 from csv import reader
 from csv import writer
+from msgpack import packb
 from datetime import datetime
 import requests
-
 
 csv.register_dialect('myDialect',
                      delimiter='|',
@@ -16,42 +16,28 @@ csv.register_dialect('myDialect',
 
 csv_file = "people.csv"
 
-def read_CSV():
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
 
-    #for row in reader:
-        #print(row)
-    return file
-
-
-def update_CSV():
-    # Open the input_file in read mode and output_file in write mode
-    with open(csv_file, 'r') as file, \
-            open('output_people.csv', 'w', newline='') as write_obj:
-        reader = csv.reader(file)
-
-        for row in reader:
-            cpr = random_cpr_generator()
-            #print(CPR)
-            csv_writer = writer(write_obj)
-            row.append(cpr)
-            csv_writer.writerow(row)
-
-
-def creat_XML():
+def Legacy_Central_System():
     # find file and open it
-    file = read_CSV()
 
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
+
         for row in reader:
             name = row[0]
             lName = row[1]
             mail = row[2]
-            cpr = random_cpr_generator()
 
-            # XML stuff
+        # Generate a CPR
+            mydate = row[3]
+            # print(date)
+            number = random.randint(1000, 9999)
+            my_date = (mydate[0] + mydate[1] + mydate[3] + mydate[4] + mydate[6] + mydate[7] + mydate[8] + mydate[9])
+            # print(my_date)
+            CPR = str(my_date) + str(number)
+            # print(CPR)
+
+        # Build an XML body
             root = minidom.Document()
 
             xml = root.createElement('person')
@@ -66,31 +52,46 @@ def creat_XML():
             xml.appendChild(lastname)
 
             cprnumber = root.createElement('CPRNumber')
-            cprnumber.setAttribute('CPRNumber', cpr)
+            cprnumber.setAttribute('CPRNumber', CPR)
             xml.appendChild(cprnumber)
 
             email = root.createElement('Email')
             email.setAttribute('Email', mail)
             xml.appendChild(email)
 
-            xml_str = root.toprettyxml(indent ="\t")
+            xml_str = root.toprettyxml(indent="\t")
 
             save_path_file = "body.xml"
 
             with open(save_path_file, "w") as f:
                 f.write(xml_str)
 
-            postRequestNemID(xml_str)
+        # Send a POST request
+            url = 'http://localhost:8080/nemID'
 
+            myobj = {xml_str}
 
-    #return print("nothing yet")
+            x = requests.post(url, data=myobj)
+            response = requests.get('http://localhost:8080/nemID')
 
+            #date = x.json()
+            print(x.status_code)
 
-def create_msgpack():
-    return print("nothing yet")
+        # Write msgpack file
+
+            data = "f_name: " + row[0] + "," + "l_name: " + row[1] + "," + "birth_date: " \
+                   + row[3] + "," + "email: " + row[2] + "," + "country: " + row[6] + "," + "phone: " \
+                   + row[4] + "," + "address: " + row[5] + "," + "CPR: " + str(CPR) + "," + "NemID: " + nemid
+
+            if os.path.exists("../" + str(CPR) + ".msgpack"):
+                return print("nothing")
+            else:
+                with open("../msgpack/" + str(CPR) + ".msgpack", "wb") as outfile:
+                    packed = packb(data)
+                    outfile.write(packed)
+
 
 def postRequestNemID(xmlBody):
-
     url = 'http://localhost:8080/nemID'
 
     myobj = {xmlBody}
@@ -99,23 +100,8 @@ def postRequestNemID(xmlBody):
 
     date = x.json()
 
-    #return print("nothing yet")
-
-def random_cpr_generator():
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-
-        for row in reader:
-            date = row[3]
-            #print(date)
-            number = random.randint(1000, 9999)
-            my_date = (date[0] + date[1] + date[3] + date[4] + date[6] + date[7] + date[8] + date[9])
-            #print(my_date)
-            CPR = str(my_date) + str(number)
-            #print(CPR)
-    return CPR
-
-#update_CSV()
+    # return print("nothing yet")
 
 
-creat_XML()
+Legacy_Central_System()
+
